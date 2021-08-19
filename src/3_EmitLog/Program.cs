02824fb05dc4045fb7fa4c233dc37c2d57f9ce0e
LangBui <bnlinhlang@gmail.com>
2021-08-19 12:01:34 +0700

    Add connection helper

--- src/3_EmitLog/Program.cs
+++ src/3_EmitLog/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using Shared;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        var factory = new ConnectionFactory() { HostName = "localhost" };
+        //var factory = new ConnectionFactory() { HostName = "localhost" };
+        var factory = ConnectionHelper.Factory;
+
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
